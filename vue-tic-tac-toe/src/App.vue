71af6255352e8985f205a7ebcf75598964777fc5
utpandey <utsavpandey8@gmail.com>
2021-01-26 22:45:36 +0530

    app changes

---
+++ vue-tic-tac-toe/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Board />
+</template>
+
+<script>
+import Board from "./components/Board";
+export default {
+  name: "App",
+  components: {
+    Board,
+  },
+};
+</script>
+
+<style>
+</style>
